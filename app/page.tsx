2a36bff91f957206c8d22583d464f876f432e18f
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-11-03 13:55:06 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.errorTitle}>Erreur Serveur</h1>
+      <p className={styles.errorMessage}>
+        L'API YouTube a du mal à répondre pour le moment. <br /> Veuillez réessayer plus tard.
+      </p>
+    </div>
+  );
+}
