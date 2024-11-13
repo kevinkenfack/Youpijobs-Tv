21b9431d9a151ddd6b3d3362ff65dac2a8286c14
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-11-13 08:51:24 +0100

    update page youtube api

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import styles from './page.module.css';
 
-export default function Home() {
+export default function Homess() {
   return (
     <div className={styles.container}>
       <h1 className={styles.errorTitle}>Erreur Serveur</h1>
